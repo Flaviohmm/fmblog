362ddf254b2ea6a491b9eb13186ee96f0191e4ad
Flaviohmm <fhenrique609@gmail.com>
2022-08-17 20:01:57 -0300

    Add Blog Categories

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,26 @@ from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('project')
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     summary = RichTextField()
     content = RichTextUploadingField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
