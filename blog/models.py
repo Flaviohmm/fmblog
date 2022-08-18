0649e014cfdbba7c445d3d66bc1afd7df656f6e5
Flaviohmm <fhenrique609@gmail.com>
2022-08-18 19:07:25 -0300

    ForeignKey Category

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     summary = RichTextField()
     content = RichTextUploadingField()
     post_date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default='coding')
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
